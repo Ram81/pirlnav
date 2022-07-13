40aa9951b7ef1341b9b849c7c58b296d6382f39a
theophilee <theophile.gervet@gmail.com>
2022-07-12 20:31:10 -0700

    eval rl checkpoint

--- job_scripts/launch_eval_jobs.sh
+++ job_scripts/launch_eval_jobs.sh
@@ -73,12 +73,28 @@ done
 SEGMENTATION=coco
 MODEL=robot_camera_settings_and_${SEGMENTATION}_detector
 EVAL_SETTINGS=(robot_camera_settings_with_noise robot_camera_settings_without_noise)
-CKPTS=("TODO")
+CKPTS=(12 14)
 
 for EVAL_SETTING in ${EVAL_SETTINGS[@]}; do
     for CKPT in ${CKPTS[@]}; do
         sbatch job_scripts/il/submit_eval_job.sh \
             $CONFIG_ROOT/il_rl/ddppo_semseg_ft_objectnav_${EVAL_SETTING}_and_${SEGMENTATION}_detector.yaml \
+            $TB_ROOT/rl_model_finetune_eval/$MODEL/$EVAL_SETTING/ckpt$CKPT \
+            $CKPT_ROOT/rl_model_finetune/$MODEL/ckpt.$CKPT.pth
+    done
+done
+
+# Original camera settings & MP3D segmentation
+
+SEGMENTATION=mp3d
+MODEL=original_camera_settings_and_${SEGMENTATION}_detector
+CKPTS=(32)
+EVAL_SETTINGS=(robot_camera_settings_with_noise robot_camera_settings_without_noise)
+
+for EVAL_SETTING in ${EVAL_SETTINGS[@]}; do
+    for CKPT in ${CKPTS[@]}; do
+        sbatch job_scripts/il/submit_eval_job.sh \
+            $CONFIG_ROOT/il_rl/ddppo_semseg_ft_objectnav_${EVAL_SETTING}_and_${SEGMENTATION}_detector_with_obs_transforms.yaml \
             $TB_ROOT/rl_model_finetune_eval/$MODEL/$EVAL_SETTING/ckpt$CKPT \
             $CKPT_ROOT/rl_model_finetune/$MODEL/ckpt.$CKPT.pth
     done
